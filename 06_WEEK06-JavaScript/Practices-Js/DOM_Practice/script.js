fd835708de1552352f249c11dfc7613a4def0e0c
Ceylan Yıldırım <yldrmceylan.1@gmail.com>
2024-07-12 17:41:50 +0300

    script.js düzenlendi

--- 06_WEEK06-JavaScript/Practices-Js/DOM_Practice/script.js
+++ 06_WEEK06-JavaScript/Practices-Js/DOM_Practice/script.js
@@ -5,8 +5,6 @@ const btnClick=document.getElementById('myButton');
 const div=document.getElementById('myDiv');
 
 //Manipulations and Functionality
-
-
 btnClick.addEventListener('click',function(){
     alert("You Clicked the Button! ");
     div.innerHTML="Div Content Changed 🤸🏽‍♀️ "; 
