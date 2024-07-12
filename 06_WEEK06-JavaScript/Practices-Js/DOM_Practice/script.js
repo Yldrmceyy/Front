bd0554620eebaeac0a33ea17a1f5fccc86a0090b
Ceylan Yıldırım <yldrmceylan.1@gmail.com>
2024-07-12 17:28:17 +0300

    Dom Manipulation was applied to the div and button.

---
+++ 06_WEEK06-JavaScript/Practices-Js/DOM_Practice/script.js
@@ -0,0 +1,15 @@
+'use strict';
+
+//Selecting Elements 
+const btnClick=document.getElementById('myButton');
+const div=document.getElementById('myDiv');
+
+//Manipulations and Functionality
+
+
+btnClick.addEventListener('click',function(){
+    alert("You Clicked the Button! ");
+    div.innerHTML="Div Content Changed 🤸🏽‍♀️ "; 
+} );
+
+
