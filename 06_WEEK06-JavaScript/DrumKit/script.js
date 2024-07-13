4d04dec7bc926f9bc46f91cdbcfa99bae656ed80
Ceylan Yıldırım <yldrmceylan.1@gmail.com>
2024-07-13 14:45:32 +0300

    //adding event listener for keys

--- 06_WEEK06-JavaScript/DrumKit/script.js
+++ 06_WEEK06-JavaScript/DrumKit/script.js
@@ -1,22 +1,28 @@
-const kits=["kick", "openhat", "snare", "tom"];
+const kits = ["kick", "openhat", "snare", "tom"];
 
-const containerEl=document.querySelector(".container");
+const containerEl = document.querySelector(".container");
 
-kits.forEach((kit)=>{
+kits.forEach((kit) => {
+  //created buttons
+  const btnEl = document.createElement("button");
+  btnEl.classList.add("btn");
+  btnEl.innerHTML = kit;
+  containerEl.appendChild(btnEl);
 
-    //created buttons
-    const btnEl= document.createElement("button");
-    btnEl.classList.add("btn");
-    btnEl.innerHTML=kit;
-    containerEl.appendChild(btnEl);
+  //created audio
+  const audioEl = document.createElement("audio");
+  containerEl.appendChild(audioEl);
+  audioEl.src = "./sounds/" + kit + ".wav";
 
-    //created audio 
-    const audioEl=document.createElement("audio");
-    containerEl.appendChild(audioEl);
-    audioEl.src="./sounds/" + kit + ".wav";
-   
-    btnEl.addEventListener('click', ()=>{
-        audioEl.play();
+  //Adding event listener
+  btnEl.addEventListener("click", () => {
+    audioEl.play();
+  });
 
-    })
+  //adding event listener for keys
+  window.addEventListener("keydown", (e) => {
+    if (e.key === kit.slice(0, 1)) {
+      audioEl.play();
+    }
+  });
 });
