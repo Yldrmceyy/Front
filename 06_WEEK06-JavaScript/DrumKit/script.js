07abe3df3c088fc996c37ba4783ed236c1e42c3e
Ceylan Yıldırım <yldrmceylan.1@gmail.com>
2024-07-13 14:30:51 +0300

    created audio with javascript

--- 06_WEEK06-JavaScript/DrumKit/script.js
+++ 06_WEEK06-JavaScript/DrumKit/script.js
@@ -3,10 +3,15 @@ const kits=["kick", "openhat", "snare", "tom"];
 const containerEl=document.querySelector(".container");
 
 kits.forEach((kit)=>{
+
+    //created buttons
     const btnEl= document.createElement("button");
     btnEl.classList.add("btn");
     btnEl.innerHTML=kit;
     containerEl.appendChild(btnEl);
 
-    
+    //created audio 
+    const audioEl=document.createElement("audio");
+    containerEl.appendChild(audioEl);
+    audioEl.src="sounds/" + kit + ".wav";
 });
