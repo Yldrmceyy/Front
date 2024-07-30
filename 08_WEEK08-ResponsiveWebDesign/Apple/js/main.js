bb2fa199a47df4c897c74a9a19e3ec9a8f98cc84
Ceylan Yıldırım <yldrmceylan.1@gmail.com>
2024-07-30 22:15:14 +0300

    Implement Swiper slider for the camera section

---
+++ 08_WEEK08-ResponsiveWebDesign/Apple/js/main.js
@@ -0,0 +1,10 @@
+(function(){
+    var camera=new Swiper('#camera .swiper',{
+        speed:600,
+        spaceBetween:12,
+        navigaiton:{
+            prevEl:'.swiper-button-prev',
+            nextEl:'.swiper-button-next',
+        }
+    })
+})();
